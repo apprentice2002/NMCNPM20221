12f7c3f7066731f8ad04136c64845c514414b844
Lữ Thanh Tùng <103517244+tunglt2k2@users.noreply.github.com>
2023-02-19 23:20:29 +0700

    up

--- src/main/java/com/cnpm/Main.java
+++ src/main/java/com/cnpm/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/cnpm/chuc-nang-view/trang-chu-chuc-nang-view/dang-nhap.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 320);
+        Scene scene = new Scene(fxmlLoader.load(), 629, 383);
         stage.setTitle("");
         stage.setScene(scene);
         stage.show();
